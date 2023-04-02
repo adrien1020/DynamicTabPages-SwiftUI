85cd2bd6a0ad4e3b0c57b95c366d1fcaa70aed89
Adrien SURUGUE <adrien.surugue@gmail.com>
2023-04-02 16:14:56 +0200

    Initial Commit

---
+++ DynamicTabPages/DynamicTabPagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicTabPagesApp.swift
+//  DynamicTabPages
+//
+//  Created by Adrien Surugue on 02/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct DynamicTabPagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
